ec1330b509d1400685eebd91267e3fe0d91bc8ab
zmulholland <zack-mulholland@uiowa.edu>
2026-03-10 14:40:28 -0500

    settings page complete

--- src/pages/SettingsPage/SettingsPage.js
+++ src/pages/SettingsPage/SettingsPage.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import { Button, Stack, Typography } from '@mui/material';
 import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
+import LogoutIcon from '@mui/icons-material/Logout';
 import './SettingsPage.css';
 
 function SettingsPage({user, signOut}) {
   return (
     <div className='settings'>
-      <Stack direction="row" spacing={2} sx={{ justifyContent: 'center', width: '100%' }}>
-        <AccountCircleRoundedIcon sx={{ fontSize: 100, color: "#EEBE02", bgcolor: "#000000" }}/>
-        <Stack direction="column">
-          <Typography variant="h4" component="h1">
+      <Stack direction="row" spacing={4} sx={{ justifyContent: 'center', width: '100%' }}>
+        <AccountCircleRoundedIcon sx={{ fontSize: 150, color: "#EEBE02" }}/>
+        <Stack direction="column" width={400} justifyContent="center">
+          <Typography variant="h4" component="h1" color="#000000">
           {user?.username ?? user?.signInDetails?.loginId ?? 'User'}
           </Typography>
         </Stack>
       </Stack>
-      <Button onClick={signOut}>
-        Log Out
-      </Button>
+      <Stack direction="row" sx={{ width: '65%', justifyContent: 'flex-end' }}>
+        <Button 
+          onClick={signOut} 
+          endIcon={<LogoutIcon />}
+          sx={{ color: "#000000", bgcolor: "#EEBE02", width: 150, borderRadius: 16 }}
+        >
+          Log Out
+        </Button>
+      </Stack>
     </div>
   );
 }
